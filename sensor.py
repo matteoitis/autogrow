fba099c3e67ec02e9ddfc9257d9e91c964fe7641
matteoitis <matteo@borgio.li>
2024-12-02 14:31:00 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,12 +1,11 @@
+from adafruit_ads1x15.analog_in import AnalogIn
+
 class Sensor:
-    def __init__(self, i2c, pin):
-        self.i2c = i2c
-        self.pin = pin
+    def __init__(self, ads, pin):
+        self.sensor_channel = AnalogIn(ads, pin)
 
     def read_raw_value(self):
-        # Implement the logic to read raw value from the sensor
-        return 0
+        return self.sensor_channel.value
 
     def read_voltage(self):
-        # Implement the logic to convert raw value to voltage
-        return 0.0
+        return self.sensor_channel.voltage
